a5c9258c50057b8c130ade3bd49687b014e30979
Sébastien <sebastien@shyrogan.fr>
2024-11-30 23:13:13 +0100

    feat: more fixes

--- frontend/instrumentation.ts
+++ frontend/instrumentation.ts
@@ -1,8 +1,11 @@
 import { registerOTel } from '@vercel/otel'
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http"
 
 export function register() {
   registerOTel({
     serviceName: 'frontend',
-    
+    traceExporter: new OTLPTraceExporter({
+      url: process.env.OTEL_EXPORTER_OTLP_LOGS_ENDPOINT
+    })
   })
 }
